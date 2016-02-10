19aac123574d48eb3c453756a4196d5e040fbef8
platy11 <mightymaxmax@gmail.com>
2016-02-10 20:17:33 +1100

    Updates

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+
 var Color = net.brehaut.Color;
+
+a = function () {
+  document.getElementById('color').addEventListener('input', function () {
+    document.querySelector('body').setAttribute('style', 'background-color: ' + document.getElementById('color').value);
+  });
+};
