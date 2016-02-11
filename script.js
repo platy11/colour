9572f2b6dd31cfdfc855dcdee6629ffbcf47d6e7
platy11 <mightymaxmax@gmail.com>
2016-02-11 19:34:03 +1100

    Total rework

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ var Color = net.brehaut.Color;
 
 a = function () {
   document.getElementById('color').addEventListener('input', function () {
-    document.querySelector('body').setAttribute('style', 'background-color: ' + document.getElementById('color').value);
+    if(Color(document.getElementById('color').value).red == undefined) {
+
+    } else {
+      document.querySelector('body').style.backgroundColor = document.getElementById('color').value;
+    };
   });
 };
+
+window.onload = function(){a();};
