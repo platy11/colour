7ee08465955b9a1ba733aaa7639c6123bc4a6135
platy11 <mightymaxmax@gmail.com>
2016-02-11 20:15:55 +1100

    Start on colour alternatives

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ a = function () {
     if(Color(document.getElementById('color').value).red == undefined) {
 
     } else {
-      document.querySelector('body').style.backgroundColor = document.getElementById('color').value;
+      var a = Color(document.getElementById('color').value)
+
+      document.querySelector('body').style.backgroundColor = a.toCSS();
+
+      document.querySelector('.a').style.backgroundColor = a.darkenByRatio(0.75).toCSS();
+
+      document.querySelector('.b').style.backgroundColor = a.darkenByRatio(0.5).toCSS();
+
+      document.querySelector('.c').style.backgroundColor = a.darkenByRatio(0.25).toCSS();
     };
   });
 };
